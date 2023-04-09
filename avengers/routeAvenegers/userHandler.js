a79ada091ecf328390c54573cac25f1a9a806899
shovon <mohammad.moniruzzaman@brainstation-23.com>
2023-04-09 13:23:52 +0600

    User journey complete

--- avengers/routeAvenegers/userHandler.js
+++ avengers/routeAvenegers/userHandler.js
@@ -101,7 +101,57 @@ handler._users.post = (requestProperties, callback) => {
 };
 
 handler._users.put = (requestProperties, callback) => {
+    const firstName = typeof(requestProperties.body.firstName) === 'string' && requestProperties.body.firstName.trim().length > 0 ? 
+    requestProperties.body.firstName : false;
 
+    const lastName = typeof(requestProperties.body.lastName) === 'string' && requestProperties.body.lastName.trim().length > 0 ? 
+        requestProperties.body.lastName : false;
+
+    const password = typeof(requestProperties.body.password) === 'string' && requestProperties.body.password.trim().length > 7 ? 
+        requestProperties.body.password : false;
+
+    const mobileNumber = typeof(requestProperties.body.mobileNumber) === 'string' && requestProperties.body.mobileNumber.trim().length === 11 ? 
+    requestProperties.body.mobileNumber : false;
+
+    if(mobileNumber) {
+        if(firstName && lastName && password) {
+        // User must not be existed
+        data.read('users', mobileNumber, (err1, uData) => {
+        const userData = {... parseJSON(uData)};
+        if(!err1 && userData) {
+            if(firstName) {
+                userData.firstName = firstName;
+            }
+            if(lastName) {
+                userData.lastName = lastName;
+            }
+            if(password) {
+                userData.password = hash(password);
+            }
+            data.update('users', mobileNumber, userData, (err2) => {
+                if(!err2) {
+                    callback(200, {
+                        message: "User updated successfully"
+                    });
+                } else {
+                    callback(500, {
+                        error: "Something went wrong"
+                    });
+                }
+            })
+        } else {
+            callback(500, {
+            error: "There is a problem in your server side"
+        });
+        }
+        });
+
+    } else {
+        callback(400, {
+        error: "You have a validation problem in your request",
+    });
+    }
+    }
 };
 
 handler._users.patch = (requestProperties, callback) => {
@@ -109,7 +159,34 @@ handler._users.patch = (requestProperties, callback) => {
 };
 
 handler._users.delete = (requestProperties, callback) => {
+    const mobileNumber = typeof(requestProperties.queryString.mobileNumber) === 'string' && requestProperties.queryString.mobileNumber.trim().length === 11 ? 
+    requestProperties.queryString.mobileNumber : false;
 
+    if(mobileNumber) {
+        data.read('users', mobileNumber, (err) => {
+            if(!err) {
+                data.delete('users', mobileNumber, (err1) => {
+                    if(!err1) {
+                        callback(200, {
+                            message: "User deleted successfully"
+                        })
+                    } else {
+                        callback(500, {
+                            error: 'Not valid request/ No permission to unlink file'
+                        })
+                    }
+                })
+            } else {
+                callback(404, {
+                    error: 'User not found'
+                })
+            }
+        })
+    } else {
+        callback(404, {
+            error: 'User not found'
+        })
+    }
 };
 
 module.exports = handler;
